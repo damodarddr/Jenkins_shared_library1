0c4d5e7719b2291ed2857b9cbd2fe76f3cbfa78d
Damodar <reddyd7702@gmail.com>
2023-11-29 10:58:54 +0530

    added files

--- kubeDelete.groovy
+++ kubeDelete.groovy
@@ -2,4 +2,8 @@ def call() {
     withKubeConfig(caCertificate: '', clusterName: '', contextName: '', credentialsId: 'k8s', namespace: '', restrictKubeConfigAccess: false, serverUrl: '') {
         sh "kubectl delete -f deployment.yml"
     }
+}def call() {
+    withKubeConfig(caCertificate: '', clusterName: '', contextName: '', credentialsId: 'k8s', namespace: '', restrictKubeConfigAccess: false, serverUrl: '') {
+        sh "kubectl delete -f deployment.yml"
+    }
 }
