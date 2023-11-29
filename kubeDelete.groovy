6e998815b21e4c2627d5c209342ba481df67e268
Damodar <reddyd7702@gmail.com>
2023-11-29 11:09:33 +0530

    added commit

--- kubeDelete.groovy
+++ kubeDelete.groovy
@@ -2,8 +2,4 @@ def call() {
     withKubeConfig(caCertificate: '', clusterName: '', contextName: '', credentialsId: 'k8s', namespace: '', restrictKubeConfigAccess: false, serverUrl: '') {
         sh "kubectl delete -f deployment.yml"
     }
-}def call() {
-    withKubeConfig(caCertificate: '', clusterName: '', contextName: '', credentialsId: 'k8s', namespace: '', restrictKubeConfigAccess: false, serverUrl: '') {
-        sh "kubectl delete -f deployment.yml"
-    }
 }
